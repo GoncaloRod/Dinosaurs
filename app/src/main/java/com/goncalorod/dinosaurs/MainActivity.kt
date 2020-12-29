af17e565637adc11c9a763fac364630eedf844eb
Gonçalo Rodrigues <rodriguesgonalo604@gmail.com>
2020-12-29 18:32:05 +0000

    Cloud firestore example

--- app/src/main/java/com/goncalorod/dinosaurs/MainActivity.kt
+++ app/src/main/java/com/goncalorod/dinosaurs/MainActivity.kt
@@ -2,10 +2,21 @@ package com.goncalorod.dinosaurs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Cloud firestore example
+        val db = Firebase.firestore
+
+        val message = hashMapOf(
+            "message" to "Hello!"
+        )
+
+        db.collection("messages").add(message)
     }
 }
