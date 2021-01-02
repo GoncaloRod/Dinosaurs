08ff8998c8de16a051a98b22b29668b178ca5699
Gonçalo Rodrigues <rodriguesgonalo604@gmail.com>
2021-01-02 14:44:39 +0000

    Sign in on splash screen behaviour done

--- app/src/main/java/com/goncalorod/dinosaurs/SignInActivity.kt
+++ app/src/main/java/com/goncalorod/dinosaurs/SignInActivity.kt
@@ -1,18 +1,14 @@
 package com.goncalorod.dinosaurs
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -23,16 +19,9 @@ class SignInActivity : AppCompatActivity() {
 
     private lateinit var googleSignInClient : GoogleSignInClient
 
-    private lateinit var signInButton : Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
-
-        signInButton = findViewById(R.id.button_SignIn)
-        signInButton.setOnClickListener {
-            signIn()
-        }
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
@@ -48,7 +37,12 @@ class SignInActivity : AppCompatActivity() {
         super.onStart()
 
         val currentUser = auth.currentUser
-        updateUI(currentUser)
+
+        if (currentUser == null) {
+            signIn()
+        } else {
+            redirect()
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -58,14 +52,12 @@ class SignInActivity : AppCompatActivity() {
         if (requestCode == RC_SIGN_IN) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             try {
-                // Google Sign In was successful, authenticate with Firebase
+                // Sign in success, authenticate with Firebase
                 val account = task.getResult(ApiException::class.java)!!
-                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
                 firebaseAuthWithGoogle(account.idToken!!)
             } catch (e: ApiException) {
-                // Google Sign In failed, update UI appropriately
-                Log.w(TAG, "Google sign in failed", e)
-                updateUI(null)
+                // Sign In failed
+                signInFailed()
             }
         }
     }
@@ -75,15 +67,11 @@ class SignInActivity : AppCompatActivity() {
         auth.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInWithCredential:success")
-                    val user = auth.currentUser
-                    updateUI(user)
+                    // Sign in success
+                    redirect()
                 } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithCredential:failure", task.exception)
-                    //Snackbar.make(view, "Authentication Failed.", Snackbar.LENGTH_SHORT).show()
-                    updateUI(null)
+                    // Sign in failed
+                    signInFailed()
                 }
             }
     }
@@ -93,16 +81,20 @@ class SignInActivity : AppCompatActivity() {
         startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
-    private fun updateUI(user : FirebaseUser?) {
-        if (user != null) {
-            signInButton.visibility = View.GONE
-        } else {
-            signInButton.visibility = View.VISIBLE
-        }
+    private fun redirect() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun signInFailed() {
+        Toast.makeText(this, R.string.sign_in_activity_sign_in_failed, Toast.LENGTH_LONG)
+                .show()
+
+        finishAndRemoveTask()
     }
 
     companion object {
-        private const val TAG = "GoogleActivity"
         private const val RC_SIGN_IN = 9001
     }
 }
