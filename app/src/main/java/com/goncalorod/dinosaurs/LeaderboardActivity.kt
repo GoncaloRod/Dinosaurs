4a7164cd6038892bf49a40db82d6dc63ef5be2cc
Mariana Oliveira <37218367+mari128tz@users.noreply.github.com>
2021-01-19 16:52:53 +0000

    Leaderboard to test

--- app/src/main/java/com/goncalorod/dinosaurs/LeaderboardActivity.kt
+++ app/src/main/java/com/goncalorod/dinosaurs/LeaderboardActivity.kt
@@ -2,10 +2,63 @@ package com.goncalorod.dinosaurs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListView
+import android.widget.TextView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class LeaderboardActivity : AppCompatActivity() {
+
+    var scores  = arrayListOf<Score>()
+
+    var scoresAdapter = ScoresAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leaderboard)
+        val db = Firebase.firestore
+        db.collection("scores").orderBy("score").get().addOnSuccessListener {
+
+            for (document in it.documents){
+                scores.add(Score(document.getString("player_name")!!, document.getLong("score")!!.toInt()))
+            }
+
+        }
+
+        val listViewScore = findViewById<ListView>(R.id.listView_leaderboard)
+        listViewScore.adapter = scoresAdapter
+        scoresAdapter.notifyDataSetChanged()
+    }
+
+
+    inner class ScoresAdapter : BaseAdapter(){
+        override fun getCount(): Int {
+            return scores.count()
+        }
+
+        override fun getItem(position: Int): Any {
+            return scores[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return 0
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val rowView = layoutInflater.inflate(R.layout.score_entry, parent, false)
+            val textViewPosition = rowView.findViewById<TextView>(R.id.textView_pos)
+            val textViewName = rowView.findViewById<TextView>(R.id.textView_name)
+            val textViewScore = rowView.findViewById<TextView>(R.id.textView_score)
+            textViewPosition.text = "#${position + 1}"
+            textViewName.text = scores[position].name
+            textViewScore.text = scores[position].score.toString()
+            rowView.isClickable = false
+            return rowView
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/goncalorod/dinosaurs/Score.kt
@@ -0,0 +1,13 @@
+package com.goncalorod.dinosaurs
+
+class Score {
+
+    var name : String
+    var score : Int
+
+    constructor(name : String, score : Int){
+
+        this.name = name
+        this.score = score
+    }
+}
